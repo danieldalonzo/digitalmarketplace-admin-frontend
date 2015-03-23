92ba1c17c430390664932ba6719433fc5680d4d2
Chris Hill-Scott <me@quis.cc>
2015-03-23 14:06:51 +0000

    Add a login page
    This commit replaces the basic auth with a login page and cookie-based sessions.
    
    It does not do any user management, but does use Flask's session handling.
    Session lifetime is 1 hour, and is renewed on every request.
    
    It adds a new environment variable (`DM_ADMIN_FRONTEND_COOKIE_SECRET`) which
    is used to encrypt the cookie. This will need to be set to something secret
    when deploying the app.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask.ext.bootstrap import Bootstrap
 from config import config
+from datetime import timedelta
 from .main import main as main_blueprint
 from .main.helpers.auth import requires_auth
 
@@ -19,6 +20,8 @@ def create_app(config_name):
     main_blueprint.config = application.config.copy()
 
     if application.config['AUTHENTICATION']:
+        application.secret_key = 'secret'
+        application.permanent_session_lifetime = timedelta(minutes=60)
         application.before_request(requires_auth)
 
     return application
